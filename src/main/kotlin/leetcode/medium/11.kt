5adb9a2f5569ca13fd374a7f063f230bdff0e14b
rolroralra <rolroralra@gmail.com>
2022-09-02 13:48:11 +0900

    solved: leetcode 11

---
+++ src/main/kotlin/leetcode/medium/11.kt
@@ -0,0 +1,30 @@
+package leetcode.medium
+
+import java.lang.Integer.max
+import java.lang.Integer.min
+
+fun main() {
+    println(Solution().maxArea(arrayOf(1,8,6,2,5,4,8,3,7)))
+}
+
+class Solution {
+    fun maxArea(height: Array<Int>): Int {
+        val pointers: Array<Int> = arrayOf(0, height.size - 1)
+
+        var maxArea = 0
+
+        for (i in 1..height.size) {
+            val minHeight = min(height[pointers[0]], height[pointers[1]])
+
+            maxArea = max(maxArea, minHeight * (pointers[1] - pointers[0]))
+
+            if (height[pointers[0]] < height[pointers[1]]) {
+                pointers[0]++
+            } else {
+                pointers[1]--
+            }
+        }
+
+        return maxArea
+    }
+}
