fd46f91d6cd066f29a39271f10e17dafe9977fe0
rolroralra <rolroralra@gmail.com>
2022-10-03 08:35:33 +0900

    solved: baekjoon 11051

---
+++ src/main/kotlin/baekjoon/number/11051.kt
@@ -0,0 +1,27 @@
+package baekjoon.number
+
+fun main() {
+    val inputList = readln().split(" ").map { it.toInt() }
+
+    val n = inputList[0]
+    val k = inputList[1]
+
+
+    val cache = MutableList(n + 1){ MutableList(n + 1) { 0 } }
+    println(combination2(n, k, cache))
+
+}
+
+fun combination2(n: Int, k: Int, cache: MutableList<MutableList<Int>>): Int {
+    if (cache[n][k] > 0) {
+        return cache[n][k]
+    }
+
+    if (k == 0 || k == n) {
+        cache[n][k] = 1
+        return cache[n][k]
+    }
+
+    cache[n][k] = (combination2(n - 1, k, cache) + combination2(n - 1, k - 1, cache)) % 10_007
+    return cache[n][k]
+}
