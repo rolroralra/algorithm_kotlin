d69e54602aae5f9fe18f4333b10833cfc2fe8699
rolroralra <rolroralra@gmail.com>
2022-10-05 23:51:34 +0900

    solved: baekjoon 1927, 11286

---
+++ src/main/kotlin/baekjoon/heap/11286.kt
@@ -0,0 +1,15 @@
+package baekjoon.heap
+
+import kotlin.math.abs
+
+fun main() {
+    val n = readln().trim().toInt()
+
+    val heap = Heap<Int>(comparator = { a: Int, b: Int -> if (abs(a) == abs(b)) a - b else abs(a) - abs(b)})
+    for (i in 0 until n) {
+        when (val input = readln().trim().toInt()) {
+            0 -> println(if (heap.isEmpty()) 0 else heap.poll())
+            else -> heap.add(input)
+        }
+    }
+}
---
+++ src/main/kotlin/baekjoon/heap/1927.kt
@@ -0,0 +1,14 @@
+package baekjoon.heap
+
+fun main() {
+    val n = readln().trim().toInt()
+
+    val heap = Heap<Int>()
+
+    for (i in 0 until n) {
+        when (val input = readln().trim().toInt()) {
+            0 -> println(if (heap.isEmpty()) 0 else heap.poll())
+            else -> heap.add(input)
+        }
+    }
+}
