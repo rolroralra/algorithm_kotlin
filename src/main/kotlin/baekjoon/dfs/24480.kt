696e7a584bf48ca2d742d857d3773fd450f6652f
rolroralra <rolroralra@gmail.com>
2022-10-08 15:29:33 +0900

    solved: baekjoon 24479, 24480, 24444 (dfs, bfs)

---
+++ src/main/kotlin/baekjoon/dfs/24480.kt
@@ -0,0 +1,38 @@
+package baekjoon.dfs
+
+fun main() {
+    val list = readln().trim().split(" ").map { it.toInt() }
+    val n = list[0]
+    val m = list[1]
+    val r = list[2]
+
+    val graph = mutableMapOf<Int, MutableList<Int>>()
+    for (i in 0 until m) {
+        val inputList = readln().trim().split(" ").map { it.toInt() - 1 }
+
+        graph.putIfAbsent(inputList[0], mutableListOf())
+        graph[inputList[0]]?.add(inputList[1])
+        graph.putIfAbsent(inputList[1], mutableListOf())
+        graph[inputList[1]]?.add(inputList[0])
+    }
+
+    graph.forEach { (_: Int, l: MutableList<Int>) -> l.sortWith(Comparator.reverseOrder()) }
+
+    val isVisited = (1..n).map { 0 }.toMutableList()
+    dfs2( r - 1, graph, isVisited)
+
+    isVisited.forEach{ println(it) }
+}
+
+var count2 = 1
+fun dfs2(index: Int, graph: Map<Int, List<Int>>, isVisited: MutableList<Int>) {
+    isVisited[index] = count2++
+
+    for (nextIndex in graph.getOrDefault(index, emptyList())) {
+        if (isVisited[nextIndex] > 0) {
+            continue
+        }
+
+        dfs2(nextIndex, graph, isVisited)
+    }
+}
