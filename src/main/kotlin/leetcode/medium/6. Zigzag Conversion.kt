6229ab8ccbb762d651e9f1ffabafc81e42145d62
rolroralra <rolroralra@gmail.com>
2022-10-20 22:00:10 +0900

    solved: leetcode 6. Zigzag Conversion

---
+++ src/main/kotlin/leetcode/medium/6. Zigzag Conversion.kt
@@ -0,0 +1,32 @@
+package leetcode.medium
+
+fun main() {
+    println(convert("PAYPALISHIRING", 3))
+    println(convert("PAYPALISHIRING", 4))
+    println(convert("A", 1))
+
+}
+
+fun convert(s: String, numRows: Int): String {
+    val sbArray = Array(numRows) { StringBuilder() }
+    var index = 0
+    while (index < s.length) {
+        for (i in 0 until numRows) {
+            if (index >= s.length) {
+                break
+            }
+
+            sbArray[i].append(s[index++])
+        }
+
+        for (i in numRows - 2 downTo 1) {
+            if (index >= s.length) {
+                break
+            }
+
+            sbArray[i].append(s[index++])
+        }
+    }
+
+    return sbArray.fold(StringBuilder()) { result, sb -> result.append(sb) }.toString()
+}
