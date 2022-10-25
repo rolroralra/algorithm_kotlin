58d0843ff9676519af0a39ef9dd5c47c27ad887a
rolroralra <rolroralra@gmail.com>
2022-10-25 23:24:58 +0900

    solved: leetcode 9. Palindrome Number

---
+++ src/main/kotlin/leetcode/medium/9. Palindrome Number.kt
@@ -0,0 +1,19 @@
+package leetcode.medium
+
+fun main() {
+    println(isPalindrome(121))
+    println(isPalindrome(-121))
+    println(isPalindrome(10))
+}
+
+fun isPalindrome(x: Int): Boolean {
+    if (x < 0) {
+        return false
+    }
+
+    if (x == 0) {
+        return true
+    }
+
+    return x.toBigInteger() == x.toString().reversed().trimStart('0').toBigInteger()
+}
