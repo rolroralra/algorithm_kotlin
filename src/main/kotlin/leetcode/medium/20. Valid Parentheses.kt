987d7637123586ffec715125f3d5f8f0f27b988d
rolroralra <rolroralra@gmail.com>
2022-10-28 22:25:48 +0900

    solved: leetcode 20. Valid Parentheses

---
+++ src/main/kotlin/leetcode/medium/20. Valid Parentheses.kt
@@ -0,0 +1,39 @@
+package leetcode.medium
+
+import java.util.*
+
+fun main() {
+    println(isValid("()"))
+    println(isValid("(){}[]"))
+    println(isValid("({[]})"))
+    println(isValid("(){"))
+}
+
+fun isValid(s: String): Boolean {
+    val stack = Stack<Char>()
+
+    for (c in s) {
+        if (isOpenedChar(c)) {
+            stack.push(convertToClosedChar(c))
+        } else if (isClosedChar(c)) {
+            if (stack.isEmpty() || c != stack.pop()) {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+
+    return stack.isEmpty()
+}
+
+fun convertToClosedChar(c: Char): Char = when (c) {
+    '{' -> '}'
+    '[' -> ']'
+    '(' -> ')'
+    else -> throw IllegalArgumentException("Illegal character : $c")
+}
+
+fun isOpenedChar(c: Char): Boolean = c == '{' || c == '(' || c== '['
+
+fun isClosedChar(c: Char): Boolean = c == '}' || c == ')' || c== ']'
