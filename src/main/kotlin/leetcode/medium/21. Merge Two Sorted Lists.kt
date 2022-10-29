cf5cc06d62eadce861f08124171f6ee0b77a10bb
rolroralra <rolroralra@gmail.com>
2022-10-30 00:01:18 +0900

    solved: leetcode 21. Merge Two Sorted Lists

--- src/main/kotlin/leetcode/medium/21. Merge Two Sorted Lists.kt
+++ src/main/kotlin/leetcode/medium/21. Merge Two Sorted Lists.kt
@@ -1,6 +1,24 @@
 package leetcode.medium
 
 fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+    if (list1 == null) {
+        return list2
+    }
+
+    if (list2 == null) {
+        return list1
+    }
+
+    if (list1.`val` <= list2.`val`) {
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+    }
+}
+
+fun mergeTwoLists2(list1: ListNode?, list2: ListNode?): ListNode? {
     val result = ListNode(0)
 
     var left = list1
