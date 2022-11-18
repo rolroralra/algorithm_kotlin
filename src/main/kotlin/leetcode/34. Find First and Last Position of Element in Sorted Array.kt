ac2e52d25fec6be69c58ce8a28cdec7813ff1be5
김신영 <rolroralra@gmail.com>
2022-11-19 00:41:10 +0900

    solved: leetcode 34. Find First and Last Position of Element in Sorted Array

--- src/main/kotlin/leetcode/34. Find First and Last Position of Element in Sorted Array.kt
+++ src/main/kotlin/leetcode/34. Find First and Last Position of Element in Sorted Array.kt
@@ -1,5 +1,8 @@
 package leetcode
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     println(searchRange(intArrayOf(5,7,7,8,8,10), 8).contentToString())
     println(searchRange(intArrayOf(5,7,7,8,8,10), 6).contentToString())
@@ -8,26 +11,53 @@ fun main() {
 }
 
 fun searchRange(nums: IntArray, target: Int): IntArray {
-    if (target !in nums) {
-        return intArrayOf(-1, -1)
+//    return intArrayOf(lowerBound(nums, target), upperBound(nums, target))
+    val answer = intArrayOf(Int.MAX_VALUE, Int.MIN_VALUE)
+    binarySearch(nums, target, answer = answer)
+
+    if (answer[0] == Int.MIN_VALUE || answer[1] == Int.MIN_VALUE) {
+        answer[0] = -1
+        answer[1] = -1
     }
 
-    return intArrayOf(lowerBound(nums, target), upperBound(nums, target))
+    return answer
+}
+
+fun binarySearch(nums: IntArray, target: Int, start: Int = 0, end: Int = nums.lastIndex, answer: IntArray) {
+    if (start > end) {
+        return
+    }
+
+    val mid = (start + end) / 2
+    if (nums[mid] == target) {
+        answer[0] = min(answer[0], mid)
+        answer[1] = max(answer[1], mid)
+
+        binarySearch(nums, target, start ,mid - 1, answer)
+        binarySearch(nums, target, mid + 1, end, answer)
+    } else if (nums[mid] > target) {
+        binarySearch(nums, target, start, mid - 1, answer)
+    } else {
+        binarySearch(nums, target, mid + 1, end, answer)
+    }
 }
 
 fun lowerBound(nums: IntArray, target: Int): Int {
     var start = 0
     var end = nums.lastIndex
 
-    var lowerBound = nums.size
+    var lowerBound = -1
     while (start <= end) {
         val mid = (start + end) / 2
 
         if (nums[mid] >= target) {
             end = mid - 1
-            lowerBound = mid
         } else {
             start = mid + 1
+        }
+
+        if (nums[mid] ==  target) {
+            lowerBound = mid
         }
     }
     return lowerBound
@@ -45,6 +75,9 @@ fun upperBound(nums: IntArray, target: Int): Int {
             end = mid - 1
         } else {
             start = mid + 1
+        }
+
+        if (nums[mid] ==  target) {
             upperBound = mid
         }
     }
