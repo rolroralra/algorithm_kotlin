23729ddd28523ab4d67bcbcfdb0e7c3b9ed63c33
rolroralra <rolroralra@gmail.com>
2023-05-10 00:21:30 +0900

    feat: divide and conquer 10830

---
+++ src/main/kotlin/baekjoon/divideNconquer/10830.kt
@@ -0,0 +1,45 @@
+package baekjoon.divideNconquer
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+
+    val (n, b) = br.readLine().split(" ").map { it.toLong() }.let { it[0].toInt() to it[1] }
+
+    val a = (1..n).map { br.readLine().split(" ").map { it.toInt() } }
+
+    println(a.pow(b).toMatrixString())
+}
+
+fun List<List<Int>>.multiply(other: List<List<Int>>): List<List<Int>> {
+    val result = List(size) { MutableList(other[0].size) { 0 } }
+
+    (indices).forEach { i ->
+        (other[0].indices).forEach { j ->
+            (other.indices).forEach { k ->
+                result[i][j] = (result[i][j] + this[i][k] * other[k][j]) % 1000
+            }
+        }
+    }
+
+    return result
+}
+
+fun List<List<Int>>.pow(n: Long): List<List<Int>> {
+    if (n == 1L) {
+        return this
+    }
+
+    val half = pow(n / 2)
+
+    return if (n % 2 == 0L) {
+        half.multiply(half)
+    } else {
+        half.multiply(half).multiply(this)
+    }
+}
+
+fun List<List<Int>>.toMatrixString(): String {
+    return joinToString("\n") {
+        it.map { v -> v % 1000 }.joinToString(" ")
+    }
+}
