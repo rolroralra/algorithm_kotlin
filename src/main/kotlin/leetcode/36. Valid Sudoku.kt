0b91cb0a48e7f43634ab8e213ebb0ee425cd06a8
rolroralra <rolroralra@gmail.com>
2023-08-08 23:51:30 +0900

    feat: leetcode 36. Valid Sudoku

--- src/main/kotlin/leetcode/36. Valid Sudoku.kt
+++ src/main/kotlin/leetcode/36. Valid Sudoku.kt
@@ -28,3 +28,21 @@ private fun isValidSudoku(board: Array<CharArray>): Boolean {
     )
 }
 
+private fun isValidSudoku2(board: Array<CharArray>): Boolean {
+    val seen = HashSet<String>()
+
+    for(i in 0 until 9){
+        for(j in 0 until 9){
+            var number = board[i][j]
+            if(number != '.'){
+                if(!seen.add("$number in row $i") ||
+                    !seen.add("$number in column $j") ||
+                    !seen.add("$number in block ${i/3}-${j/3}")) return false
+
+            }
+        }
+    }
+
+    return true
+}
+
