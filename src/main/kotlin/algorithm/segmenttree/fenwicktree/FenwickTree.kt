e4585d224d6152d353254828a579d2bfa6046423
rolroralra <rolroralra@gmail.com>
2026-09-26 14:27:11 +0900

    feat: implement Fenwick Tree with update and query methods

--- src/main/kotlin/algorithm/segmenttree/fenwicktree/FenwickTree.kt
+++ src/main/kotlin/algorithm/segmenttree/fenwicktree/FenwickTree.kt
@@ -1,14 +1,61 @@
 package algorithm.segmenttree.fenwicktree
 
+import kotlin.collections.toList
+import kotlin.collections.withIndex
+
 /**
  * 1-indexed Fenwick tree over [size] elements. [update] adds a diff to the existing value at [index].
  */
 class FenwickTree(private val size: Int) {
-    fun update(index: Int, diff: Int) {
-        TODO("Implement diff-based point update")
+    // base index = 1
+    private val fenwickTree: MutableList<Int> = MutableList(size + 1) { 0 }
+
+    constructor(originDataArray: Array<Int>): this(originDataArray.toList())
+
+    constructor(originDataList: List<Int>): this(originDataList.size) {
+        for ((index, value) in originDataList.withIndex()) {
+            update(index, value)
+        }
     }
 
+    /**
+     * @param index base-1 index
+     * @param diff difference for update
+     *
+     */
+    fun update(index: Int, diff: Int) {
+        var currIndex = index
+
+        while (currIndex <= size) {
+            fenwickTree[currIndex] += diff
+            currIndex += currIndex.takeLowestOneBit()
+        }
+    }
+
+    /**
+     * @param leftIndexInclusive
+     * @param rightIndexInclusive
+     *
+     * @return sum which index in range(leftIndexInclusive, rightIndexInclusive)
+     */
     fun query(leftIndexInclusive: Int, rightIndexInclusive: Int): Int {
-        TODO("Implement range-sum query")
+        return query(rightIndexInclusive) - query(leftIndexInclusive - 1)
+    }
+
+    /**
+     * @param index base-1 index
+     *
+     * @return sum which index in range(1, index)
+     */
+    private fun query(index: Int): Int {
+        var result = 0
+
+        var currIndex = index
+        while (currIndex > 0) {
+            result += fenwickTree[currIndex]
+            currIndex -= currIndex.takeLowestOneBit()
+        }
+
+        return result
     }
 }
--- src/test/kotlin/algorithm/segmenttree/fenwicktree/FenwickTreeTest.kt
+++ src/test/kotlin/algorithm/segmenttree/fenwicktree/FenwickTreeTest.kt
@@ -2,6 +2,9 @@ package algorithm.segmenttree.fenwicktree
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class FenwickTreeTest {
 
@@ -33,5 +36,18 @@ class FenwickTreeTest {
 
         assertEquals(13, tree.query(3, 3))
         assertEquals(25, tree.query(1, 5))
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePositiveInts")
+    fun `Int#takeLowestBit 는 index & (-index) 와 동일한 값을 가진다`(intValue: Int) {
+        assertEquals(intValue.and(-intValue), intValue.takeLowestOneBit())
+    }
+
+    companion object {
+        @JvmStatic
+        fun providePositiveInts(): List<Arguments> {
+            return (1..1000).map { Arguments.of(it) }
+        }
     }
 }
